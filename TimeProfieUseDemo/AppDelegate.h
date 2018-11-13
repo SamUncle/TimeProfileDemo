caba0ef8e3d99e72f11bf45d5402dc5c60c2b3a7
朱守明 <507772308@qq.com>
2018-11-13 11:26:03 +0800

    Initial Commit

---
+++ TimeProfieUseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeProfieUseDemo
+//
+//  Created by zsm on 2018/11/13.
+//  Copyright © 2018年 XinHuoKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
